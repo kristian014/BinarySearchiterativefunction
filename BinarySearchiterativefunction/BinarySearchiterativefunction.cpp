6de308f13ef9e0b2373bd51cad54352dde2c70a3
kristian usifoh <kristianusifoh@outlook.com>
2020-03-16 02:35:38 +0000

    new binarysearch

--- BinarySearchiterativefunction/BinarySearchiterativefunction.cpp
+++ BinarySearchiterativefunction/BinarySearchiterativefunction.cpp
@@ -1,10 +1,76 @@
-
-
+#include <fstream>
 #include <iostream>
+using namespace std;
 
-int main()
+
+
+
+
+
+bool binarySearch(int *list, int size, int value)
 {
-    std::cout << "Hello World!\n";
+	while (size != 0)
+	{
+		int middle = size / 2;
+		int* middleArrElementPtr = list + middle;
+		int middleArrElemValue = *middleArrElementPtr;
+		if (middleArrElemValue == value) {
+			return true;
+		}
+
+		else if (value < middleArrElemValue)
+		{
+
+			middle = (size / 2) + 1;
+			value = middle;
+			return true;
+		}
+
+		else
+			middle = (size / 2) - 1;
+		value = middle;
+		return true;
+
+
+
+	}
+
 }
 
 
+
+
+void main()
+{
+
+	int m_BinarySearch[100];
+	int *ArrayFirstElelmet = m_BinarySearch;
+
+	void LoadBinaryFile(const char filename[]);
+
+
+	ifstream infile("binarysearchIn.txt", ios::in);
+	if (infile.is_open()) {
+		for (int x = 0; x < 100; x++) {
+			infile >> m_BinarySearch[x];
+			
+
+		}
+	}
+	else
+	{
+		cout << "File not stored to array successfully" << endl;
+	}
+
+	bool result = binarySearch(ArrayFirstElelmet, sizeof(m_BinarySearch), 45);
+
+	if (result == false)
+	{
+		cout << "Element was not found" << endl;
+
+	}
+	else if (result == true)
+		cout << "Element Found" << endl;
+	cout << "This is for iterative binary search" << endl;
+
+}
